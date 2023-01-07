963689ee573e526501e8054517ccad5d7a867254
Vishnu PR <203116@ibsplc.com>
2023-01-07 15:56:24 +0530

    first commit

---
+++ src/main/java/com/i18n/main/I18nLanguageSupportApplication.java
@@ -0,0 +1,13 @@
+package com.i18n.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18nLanguageSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I18nLanguageSupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/i18n/main/I18nLanguageSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i18n.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18nLanguageSupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
