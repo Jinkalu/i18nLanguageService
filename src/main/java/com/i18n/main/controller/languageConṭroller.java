11ec7a14354da49dbc1b098b96d4e62053c312cc
Vishnu PR <203116@ibsplc.com>
2023-01-09 10:06:47 +0530

    work set up

--- src/main/java/com/i18n/main/controller/languageConṭroller.java
+++ src/main/java/com/i18n/main/controller/languageConṭroller.java
@@ -1,6 +1,7 @@
 package com.i18n.main.controller;
 
-import com.i18n.main.RequestVO;
+
+import com.i18n.main.vo.RequestVO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
