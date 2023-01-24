249814a4e307686c82d8a0293dfe6c564694f2c0
Vishnu PR <203116@ibsplc.com>
2023-01-24 19:01:03 +0530

    component first commit

---
+++ src/main/java/com/i18n/main/configs/LocalConfigs.java
@@ -0,0 +1,9 @@
+package com.i18n.main.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalConfigs {
+//    @Value("")
+}
---
+++ src/main/java/com/i18n/main/configs/TransalationCodes.java
@@ -0,0 +1,6 @@
+package com.i18n.main.configs;
+
+public class TransalationCodes {
+    public static final String EXCEPTION_CODE="exception.code";
+    public static final String EXCEPTION_MESSAGE="exception.message";
+}
